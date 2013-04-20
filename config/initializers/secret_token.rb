e42c7f9afa1eab6b3ea2b2ee7ceaa26a53d5f797
Abhishek <asharma@thoughtworks.com>
2013-04-20 18:19:27 +0530

    Abhi #0 Intializing the repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VolunteerConnect::Application.config.secret_token = 'b8a069b1de7999dc3899a605e872810c1b27c2a4745892943caae02603ccf0cb8b369486855e72b4b1bd9552dabafafc90876d6f61822deb33c6aac16a26bf7d'
